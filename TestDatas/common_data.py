a187feff9204977009cec66c67060f25a9647bbc
ljhtest2017 <ljhtest2017@163.com>
2019-07-08 14:24:39 +0800

    add

--- TestDatas/common_data.py
+++ TestDatas/common_data.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python
 # -*- coding: UTF-8 -*-
 
-url = 'http://120.79.176.157:8012/Index/login.html'
+web_url = 'http://120.79.176.157:8012/Index/login.html'
 # 公共登录账号
 user = '18684720553'
 passwd = 'python'
--- TestDatas/login_data.py
+++ TestDatas/login_data.py
@@ -5,7 +5,7 @@
 login_succ = {"username": "18566722082", "passwd": "python", "check": "我的账户[小小蜂96027]"}
 
 # 登录失败，无用户名，无密码，用户名格式不正确
-login_fail = [
+login_noData = [
     {"username": "", "passwd": "python", "check": "请输入手机号"},
     {"username": "18566722082", "passwd": "", "check": "请输入密码"},
     {"username": "185667220823", "passwd": "python", "check": "请输入正确的手机号"}
